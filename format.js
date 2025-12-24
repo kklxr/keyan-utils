1d396aaaf14ad7d43804126e677b51a6f5d10328
keyan <keyan@wps.cn>
2025-12-24 17:55:20 +0800

    feat:test1

--- format.js
+++ format.js
@@ -1,14 +1,14 @@
+console.log(111111);
 
 // b. format.js 格式化文件
 
 // 解决toFixed保留小数的问题
 const formatToFixed = (money, decimals = 2) => {
   return (
-    Math.round(
-      (parseFloat(money) + Number.EPSILON) * Math.pow(10, decimals)
-    ) / Math.pow(10, decimals)
+    Math.round((parseFloat(money) + Number.EPSILON) * Math.pow(10, decimals)) /
+    Math.pow(10, decimals)
   ).toFixed(decimals);
-}
+};
 const Format = {
   // 格式化金额展示： 12341234.246 -> $ 12,341,234.25
   formatMoney: (money, symbol = "", decimals = 2) =>
